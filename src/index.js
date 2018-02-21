fe9d796ac80132e9080b0cbc82c3b529467b86c1
KirylNik <ultramylo@gmail.com>
2018-02-21 18:54:27 +0300

    compleeted

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let bracketsAsString = [];
+    for (let i = 0; i < bracketsConfig.length; i++) {                       //Добавить в массив 'bracketsAsString' все
+        bracketsAsString.push(bracketsConfig[i][0] + bracketsConfig[i][1]); //переданные в 'bracketsConfig' типы скобок.
+    };
+
+    for (let z = 0; z < bracketsAsString.length + 1; z++) { //Для каждой пар скобок, переданных в 'bracketsConfig'.
+
+        for (let i = 0; i < bracketsAsString.length; i++) {  //Для каждой пар скобок, переданных в 'bracketsConfig'.
+            for (let y = 0; y < str.length; y++) {
+                if (str.indexOf(bracketsAsString[i]) != -1) { //Если в строке есть искомая по 'bracketsAsString[i]' пара.
+                    let deletePosition = str.indexOf(bracketsAsString[i]); // Записать индекс начала парной скобки в 'str'
+                    str = str.substring(0, deletePosition) + str.substring(deletePosition + 2); // Перезаписать 'str'без
+                    y--;                                                                 // найденой пары скобок.
+                };
+            };
+        };
+
+    };
+
+    if (str.length == 0) {   // Если строка пустая, то значит все элементы в 'str' в результате
+        return (true);       // последовательно удаления были парами скобок.
+    }
+    else {
+        return (false);      // Если строка осталась НЕ пустая, то значит НЕ все элементы в 'str' в результате
+    };                       // последовательно удаления были именно парами скобок и остались не пары.
+};
